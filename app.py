c6b429877ee31e37029817d58016faea905253c1
Hao Xu <hxu19@tulane.edu>
2025-04-03 12:05:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ import pandas as pd
 st.title("My Streamlit App")
 
 if st.button("Click me!"):
-    data= pd.read_csv("data.csv")
+    file = st.file_uploader("Upload a CSV file", type=["csv"])
+    if file is not None:
+        data = pd.read_csv(file)
     st.write(data)
