19f9c0db3cf3d2c01941a4b3a8914a3af63228fe
Hao Xu <hxu19@tulane.edu>
2025-04-03 12:06:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ if st.button("Click me!"):
     file = st.file_uploader("Upload a CSV file", type=["csv"])
     if file is not None:
         data = pd.read_csv(file)
-    st.write(data)
+
+        st.write(data)
