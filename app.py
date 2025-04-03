154940c34fe1f1f7f6b05975220e94ac6504d968
Hao Xu <hxu19@tulane.edu>
2025-04-03 12:02:10 -0500

    adding requirements

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+
 st.title("My Streamlit App")
-st.write("Hello, world!")
-st.button("Click me!")
+
+if st.button("Click me!"):
+    data= pd.read_csv("data.csv")
+    st.write(data)
